a4fb8781420e69c3699e08753b27dd73f02f60f7
Andre <andrelageu0607@gmail.com>
2025-07-23 18:31:39 -0500

    vista de prestamos listo

--- src/app/paginas/cuenta/cuenta.ts
+++ src/app/paginas/cuenta/cuenta.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Subscription } from 'rxjs';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
 
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.html',
-  styleUrls: ['./cuenta.scss'], 
-  imports: [RouterModule, ReactiveFormsModule, CommonModule]
+  styleUrls: ['./cuenta.scss'],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }]
 })
 export class CuentaComponent implements OnInit {
   usuario: any = null;
@@ -21,7 +25,7 @@ export class CuentaComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {
     const navigation = this.router.getCurrentNavigation();
     const stateUser = navigation?.extras?.state?.['usuario'];
-    
+
     if (stateUser) {
       this.usuario = stateUser;
       this.reservas = stateUser.reservas || [];
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, catchError, map, Observable, tap, throwError } from 'rxjs';
+import { UsuariosService } from './usuarios';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private apiUrl = 'http://localhost:8080/api/auth/login';
-  private userSubject = new BehaviorSubject<any>(this.getUsuario());
+  public userSubject = new BehaviorSubject<any>(this.getUsuario());
   public user$ = this.userSubject.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private usuarioService: UsuariosService) { }
 
   login(username: string, password: string): Observable<void> {
     return this.http.post<any>(this.apiUrl, { username, password }).pipe(
@@ -18,7 +19,7 @@ export class AuthService {
         this.userSubject.next(user);
         this.router.navigate(['/cuenta']);
       }),
-      map(() => {}),
+      map(() => { }),
       catchError(error => {
         console.error('Login error:', error);
         return throwError(() => error);
@@ -42,14 +43,30 @@ export class AuthService {
   }
 
   actualizarUsuarioConReserva(nuevaReserva: any): void {
-  const usuarioActual = this.getUsuario();
-  if (usuarioActual) {
-    const usuarioActualizado = {
-      ...usuarioActual,
-      reservas: [...(usuarioActual.reservas || []), nuevaReserva]
-    };
-    localStorage.setItem('usuario', JSON.stringify(usuarioActualizado));
-    this.userSubject.next(usuarioActualizado);
+    const usuarioActual = this.getUsuario();
+    if (usuarioActual) {
+      const usuarioActualizado = {
+        ...usuarioActual,
+        reservas: [...(usuarioActual.reservas || []), nuevaReserva]
+      };
+      localStorage.setItem('usuario', JSON.stringify(usuarioActualizado));
+      this.userSubject.next(usuarioActualizado);
+    }
   }
-}
+
+  actualizarUsuarioDesdeBackend(): void {
+    const usuario = this.getUsuario();
+    if (!usuario?.id) return;
+
+    this.usuarioService.getUsuario(usuario.id).subscribe({
+      next: (usuarioActualizado) => {
+        localStorage.setItem('usuario', JSON.stringify(usuarioActualizado));
+        this.userSubject.next(usuarioActualizado);
+      },
+      error: (err) => {
+        console.error('Error actualizando usuario desde backend:', err);
+      }
+    });
+  }
+
 }
